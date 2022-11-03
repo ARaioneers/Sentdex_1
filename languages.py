c4fc6b738ca1b7e363b363787b23fcc541f2f3b6
Anjali <anjali.rajmane@aioneers.com>
2022-11-03 11:52:19 +0530

    New repo for sentdex

---
+++ languages.py
@@ -0,0 +1,9 @@
+programming_languages="python","Java","C++","C"
+print(type(programming_languages))
+for lang in programming_languages:
+    print(lang)
+    
+if len(programming_languages)> 1:
+    print(f"Number of languages = {len(programming_languages)}")
+else:
+    pass
